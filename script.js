d26219a064c2f768129f8acf60c44fb29335fedf
Kenneth Ramos <52458739+fannyxmikasa@users.noreply.github.com>
2022-09-21 17:23:43 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -6,9 +6,9 @@ console.log(json['user_pic'])
 console.log(json['user'])
 console.log(json['likes'])
 //document.querySelector('#txt').innerHTML = text
-document.querySelector('#post_img').src = `api.scraperapi.com/?api_key=010e6bdc516d1ce035d6eeb741d5ac45&url=${json['post_img']}`
-document.querySelector('#user_pic1').src = `api.scraperapi.com/?api_key=010e6bdc516d1ce035d6eeb741d5ac45&url=${json['user_pic']}`
-document.querySelector('#user_pic2').src = `api.scraperapi.com/?api_key=010e6bdc516d1ce035d6eeb741d5ac45&url=${json['user_pic']}`
+document.querySelector('#post_img').src = `http://api.scraperapi.com/?api_key=010e6bdc516d1ce035d6eeb741d5ac45&url=${json['post_img']}`
+document.querySelector('#user_pic1').src = `http://api.scraperapi.com/?api_key=010e6bdc516d1ce035d6eeb741d5ac45&url=${json['user_pic']}`
+document.querySelector('#user_pic2').src = `http://api.scraperapi.com/?api_key=010e6bdc516d1ce035d6eeb741d5ac45&url=${json['user_pic']}`
 document.querySelector('#user_name1').innerHTML = json['user']
 document.querySelector('#user_name2').innerHTML = json['user']
 document.querySelector('#like_count').innerHTML = json['likes']
